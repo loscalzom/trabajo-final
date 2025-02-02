ba7461d5b118e9fc2699bdc844741cc81ac7e7cd
loscalzom <marceloscalzo@gmail.com>
2025-02-02 13:31:32 -0300

    nuevo context

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,28 +1,3 @@
-/* import { createContext, useEffect, useState } from "react"
-
-//Crear el contexto
-export const AuthContext = createContext()
-
-
-//Crear el proveedor de contexto (Es un componente)
-
-export const AuthContextProvider = ({children}) =>{
-
-    const [isAuthenticatedState, setIsAuthenticatedState] = useState(Boolean(sessionStorage.getItem('access_token')))
-    const login = (access_token) =>{
-        sessionStorage.setItem('access_token', access_token)
-        setIsAuthenticatedState(true)
-    }
-  
-
-    return (
-        <AuthContext.Provider value={{isAuthenticatedState, login}}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
- */
-
 import { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
@@ -34,22 +9,26 @@ export const AuthContextProvider = ({ children }) => {
 
   useEffect(() => {
     const storedUser = JSON.parse(sessionStorage.getItem("user"));
-    const storedWorkspace = JSON.parse(sessionStorage.getItem("workspace"));
+    const storedWorkspace = sessionStorage.getItem("workspace");
 
+    // Verifica si el valor de workspace existe antes de intentar parsearlo
     if (storedUser) setUser(storedUser);
-    if (storedWorkspace) setWorkspace(storedWorkspace);
+    if (storedWorkspace) {
+      try {
+        const parsedWorkspace = JSON.parse(storedWorkspace);
+        setWorkspace(parsedWorkspace);
+      } catch (error) {
+        console.error("Error al parsear el workspace:", error);
+      }
+    }
   }, []);
 
-
- 
   const login = (access_token, userData, workspaceData) => {
-    console.log("Datos del workspace antes de llamar a login:", workspaceData)
-
     sessionStorage.setItem("access_token", access_token);
     sessionStorage.setItem("user", JSON.stringify(userData));
     sessionStorage.setItem("workspace", JSON.stringify(workspaceData));
 
-    console.log("Workspace guardado en sessionStorage:", sessionStorage.getItem("workspace"))
+    console.log("Workspace guardado en sessionStorage:", workspaceData);
 
     setIsAuthenticatedState(true);
     setUser(userData);
@@ -79,24 +58,3 @@ export const AuthContextProvider = ({ children }) => {
 
 
 
-
-
-
-/* export const AuthContext = createContext()
-
-const AuthContextProvider = ({children}) => {
-    let isAuthenticated = Boolean(sessionStorage.getItem('access_token'))
-    const [isAuthenticatedState, setIsAuthenticatedState] = useState(isAuthenticated)
-
-    useEffect(() =>{
-        const auth_token = sessionStorage.getItem('access_token')
-        if(auth_token) {
-            setIsAuthenticatedState(true)
-        }
-    }, [])
-    return ( 
-        <AuthContext.Provider value={{isAuthenticatedState}}>
-            {children}
-        </AuthContext.Provider>
-    )
-} */
