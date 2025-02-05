7ef86ba0380ce1a8845ef0b10fd05d5c66fa8109
loscalzom <marceloscalzo@gmail.com>
2025-02-05 19:13:11 -0300

    mostrando errores

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,46 +1,41 @@
-import React, { useContext } from 'react'
-import useForm from '../hooks/useForm'
-import ENVIROMENT from '../utils/constants/enviroment'
-import { Link, useNavigate } from 'react-router-dom'
-import { AuthContext } from '../Context/AuthContext'
-import '../css/loginScreen.css'
+import React, { useContext, useState } from 'react';
+import useForm from '../hooks/useForm';
+import ENVIROMENT from '../utils/constants/enviroment';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthContext';
+import '../css/loginScreen.css';
 
 const LoginScreen = () => {
+    const { login, isAuthenticatedState } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { form_state, handleChangeInput } = useForm({ email: "", password: "" });
 
+    const [errorMessage, setErrorMessage] = useState(''); // Estado para el mensaje de error
 
-    const { login, isAuthenticatedState } = useContext(AuthContext)
-
-    const navigate = useNavigate()
-
-    const { form_state, handleChangeInput } = useForm({ email: "", password: "" })
-
-    const url = new URLSearchParams(window.location.search)
-    if (url.get("verified")) { alert("Cuenta verificada") }
+    const url = new URLSearchParams(window.location.search);
+    if (url.get("verified")) { alert("Cuenta verificada"); }
 
     const handleSubmitForm = async (event) => {
-
-        event.preventDefault()
+        event.preventDefault();
         try {
             const res = await fetch(ENVIROMENT.API_URL + "/api/auth/login", {
                 method: "POST",
                 headers: {
                     'Content-Type': 'application/json'
-
                 },
                 body: JSON.stringify(form_state)
-            })
-            const data = await res.json()
+            });
+            const data = await res.json();
 
-            console.log("Datos recibidos de la API:", data)
-
+            console.log("Datos recibidos de la API:", data);
 
             if (data.data && data.data.access_token) {
                 const userData = data.data.user_info;
                 const workspaceData = data.data.workspace;
 
-                console.log("User data:", userData)
-                console.log("Workspace data:", workspaceData)
-                console.log("Datos completos de la respuesta:", data.data)
+                console.log("User data:", userData);
+                console.log("Workspace data:", workspaceData);
+                console.log("Datos completos de la respuesta:", data.data);
 
                 if (userData && workspaceData) {
                     sessionStorage.setItem('user', JSON.stringify(userData));
@@ -49,70 +44,59 @@ const LoginScreen = () => {
                     console.error("Error: userData o workspaceData son undefined");
                 }
 
-
-                sessionStorage.setItem('token', data.data.access_token)
-                
-
+                sessionStorage.setItem('token', data.data.access_token);
                 console.log('Token guardado:', sessionStorage.getItem('token'));
                 console.log('User guardado:', sessionStorage.getItem('user'));
-                console.log('Workspace guardado:', sessionStorage.getItem('workspace'))
+                console.log('Workspace guardado:', sessionStorage.getItem('workspace'));
 
-                login(data.data.access_token, userData, workspaceData)
-                navigate("/home")
+                login(data.data.access_token, userData, workspaceData);
+                navigate("/home");
             } else {
-                console.error("Token no encontrado en la respuesta")
+                setErrorMessage("Token no encontrado en la respuesta"); // Muestra el mensaje de error
             }
 
-
-
         } catch (error) {
-            console.error("Error al loguear", error)
+            setErrorMessage("Error al iniciar sesión. Por favor, verifica tu correo y contraseña."); // Muestra el mensaje de error
+            console.error("Error al loguear", error);
         }
-
-
-
     }
-
 
     const errores = {
-        email: [
-        ],
+        email: [],
         password: []
+    };
 
-    }
+    form_state.email && form_state.email.length > 30 && errores.email.push("El límite de caracteres es 30");
+    form_state.email && form_state.email.length < 5 && errores.email.push("El mínimo de caracteres es 5");
 
-    form_state.email && form_state.email.length > 30 && errores.email.push("El límite de caracteres es 30")
-    form_state.email && form_state.email.length < 5 && errores.email.push("El mínimo de caracteres es 5")
-
-    form_state.password && form_state.password.length > 30 && errores.password.push("El máximo de caracteres es 30")
-    form_state.password && form_state.password.length < 5 && errores.password.push("El mínimo de caracteres es 5")
-
+    form_state.password && form_state.password.length > 30 && errores.password.push("El máximo de caracteres es 30");
+    form_state.password && form_state.password.length < 5 && errores.password.push("El mínimo de caracteres es 5");
 
     return (
         <div className='login-container'>
             <h1>CHAT DE AMIGOS</h1>
+            {errorMessage && <p className='error-message'>{errorMessage}</p>} {/* Muestra el mensaje de error */}
             <form onSubmit={handleSubmitForm} className='login-form'>
                 <div>
                     <label htmlFor="email">Ingresa tu email:</label>
                     <input name='email' id='email' placeholder='joedoe@gmail' value={form_state.email} onChange={handleChangeInput} />
-
                     {errores.email?.map((error, index) => <p key={index} className='error'>{error}</p>)}
                 </div>
 
                 <div>
                     <label htmlFor="password">Ingresa tu contraseña:</label>
-                    <input name='password' id='password' value={form_state.password} onChange={handleChangeInput} />
-                    <button type='submit' disabled={errores.email.length > 0 || errores.password.length || !form_state.email || !form_state.password}>Iniciar sesión</button>
-                    {errores.password?.map((error, index) => <p key={index} style={{ color: "red" }}>{error}</p>)}
-
+                    <input type='password' name='password' id='password' value={form_state.password} onChange={handleChangeInput} />
+                    {errores.password?.map((error, index) => <p key={index} className='error'>{error}</p>)}
                 </div>
+
+                <button type='submit' disabled={errores.email.length > 0 || errores.password.length || !form_state.email || !form_state.password}>Iniciar sesión</button>
+
                 <span>Aún no tienes cuenta? <Link to="/register">Regístrate</Link></span>
                 <br />
                 <Link to="/forgot-password">Olvidé mi contraseña</Link>
             </form>
-
         </div>
-    )
+    );
 }
 
-export default LoginScreen
+export default LoginScreen;
